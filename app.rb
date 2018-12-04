d843337987ea66e615945b437d9fcd769f0dcee7
cbeery <git@curtbeery.com>
2018-12-03 22:08:40 -0700

    try localtime

--- app.rb
+++ app.rb
@@ -57,7 +57,7 @@ def convert_fluids_spreadsheet_rows_to_cal(cal)
 	@rows = response.values.drop(1).reverse # .drop ignores header row
 	@rows.each_with_index do |row, index|
     cal.event do |e|
-    	start = DateTime.iso8601(row[0]).to_time
+    	start = DateTime.iso8601(row[0]).to_time.localtime
       e.dtstart     = Icalendar::Values::DateTime.new(start)
       e.dtend       = Icalendar::Values::DateTime.new(start + 600) # Add 10 min (600 sec)
       e.summary 		= row[2] # What
